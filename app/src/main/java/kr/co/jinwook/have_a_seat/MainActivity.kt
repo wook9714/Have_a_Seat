41a41fab1944f716202cec5f009cd3afcdc5128c
manduking <wook9714@gmail.com>
2021-09-18 20:06:24 +0900

    firebase setting

--- app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
+++ app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.co.jinwook.have_a_seat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
