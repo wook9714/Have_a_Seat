65003c37482f3dea5d70166ae09cee0d5cfd0665
manduking <wook9714@gmail.com>
2021-09-18 19:57:37 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.jinwook.have_a_seat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
