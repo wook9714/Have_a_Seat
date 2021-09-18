52b3cb65aee02a373e3685a91debbcb65567c154
manduking <wook9714@gmail.com>
2021-09-18 21:02:18 +0900

    firebase setting

--- app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
+++ app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
@@ -1,8 +1,10 @@
 package kr.co.jinwook.have_a_seat
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -11,7 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        makeStatusbarTransparent()
+
 
 
     }
+    fun makeStatusbarTransparent(){
+        window?.decorView?.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = Color.TRANSPARENT
+    }
+
+
 }
