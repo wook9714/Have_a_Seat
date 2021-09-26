540bb50a75bd6360b247042e35a04be2ac55916a
manduking <wook9714@gmail.com>
2021-09-26 17:35:28 +0900

    메인엑티비티 버튼만들고 툴바 만드는 중

--- app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
+++ app/src/main/java/kr/co/jinwook/have_a_seat/MainActivity.kt
@@ -1,19 +1,31 @@
 package kr.co.jinwook.have_a_seat
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 import kr.co.jinwook.have_a_seat.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu,menu)
+        return true
+    }
 
     init {
         instance = this
@@ -32,6 +44,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.toolbarMain.setNavigationOnClickListener {
+            Toast.makeText(this, "Navigation Menu Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnMyOrderBills.setOnClickListener {
+            val intentGoOrderSheet = Intent(this,OrderSheet::class.java)
+            startActivity(intentGoOrderSheet)
+
+        }
+
+
+/*
 
         UIFunction.makeStatusbarTransparent(window)
 
@@ -41,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("tag",UIFunction.getStatusBarHeight(resources).toString())
 
 
-
+*/
 
 
     }
