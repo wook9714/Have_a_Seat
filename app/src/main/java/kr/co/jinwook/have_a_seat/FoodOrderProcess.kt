28be9777d3a2fd7259b0ef5e089d97ba9d8b55f5
Dothoven98 <lvhds2012@ologgames.com>
2021-12-28 22:10:20 +0900

    dynamic view create

--- app/src/main/java/kr/co/jinwook/have_a_seat/FoodOrderProcess.kt
+++ app/src/main/java/kr/co/jinwook/have_a_seat/FoodOrderProcess.kt
@@ -2,34 +2,56 @@ package kr.co.jinwook.have_a_seat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_food_order_process.view.*
+import kotlinx.android.synthetic.main.container_in_foodorderprocess.view.*
+import kotlinx.android.synthetic.main.foodorderprocess_friendbtn.view.*
+import kotlinx.android.synthetic.main.menu_detail_select.view.*
 import kr.co.jinwook.have_a_seat.databinding.ActivityFoodOrderProcessBinding
+import kr.co.jinwook.have_a_seat.databinding.ContainerInFoodorderprocessBinding
+import kr.co.jinwook.have_a_seat.databinding.FoodorderprocessFriendbtnBinding
 import kr.co.jinwook.have_a_seat.databinding.MenuDetailSelectBinding
 
 class FoodOrderProcess : AppCompatActivity() {
     val binding by lazy{ActivityFoodOrderProcessBinding.inflate(layoutInflater)}
+    val foodDataList:ArrayList<FoodContainerData> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        var index = 0
-
+        loadData()
+        for(i in foodDataList){
+            addContainerInFoodOrderProcessing(i)
+        }
         binding.button.setOnClickListener {
-            index++
+            val tmp = FoodorderprocessFriendbtnBinding.inflate(LayoutInflater.from(this),binding.friendList,false).root
 
+            binding.friendList.addView(tmp)
 
-
-
-            binding.testLayout.addView(MenuDetailSelectBinding.inflate(layoutInflater).linearLayout)
         }
 
+    }
+    fun loadData(){
+        for(i in 1..10){
+            foodDataList.add(FoodContainerData("foodName${i}","foodInfo${i}",i*1000))
 
+        }
+    }
 
+    fun addContainerInFoodOrderProcessing(data:FoodContainerData){
+        val container = ContainerInFoodorderprocessBinding.inflate(LayoutInflater.from(this),binding.testLayout,false).root
+        container.text_foodName.text = data.foodName
+        container.text_foodInfo.text = data.foodInfo
+        container.text_price.text = data.price.toString()
 
+        binding.testLayout.addView(container)
     }
 
 }
+
+data class FoodContainerData(var foodName:String,var foodInfo:String, var price:Int)
+
