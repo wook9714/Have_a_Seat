d1ff1dbb8efc89ccec51b0028202fce5e5110cbd
jinwooklee <teamforqko@gmail.com>
2022-01-07 18:12:38 +0900

    restaurantListAct

--- app/src/main/java/kr/co/jinwook/have_a_seat/RestaurantListActivity.kt
+++ app/src/main/java/kr/co/jinwook/have_a_seat/RestaurantListActivity.kt
@@ -2,10 +2,17 @@ package kr.co.jinwook.have_a_seat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.jinwook.have_a_seat.databinding.ActivityRestaurantListBinding
 
 class RestaurantListActivity : AppCompatActivity() {
+    val TAG : String = "로그"
+    val binding by lazy { ActivityRestaurantListBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restaurant_list)
+        setContentView(binding.root)
+
     }
+
+
 }
